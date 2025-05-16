7c8c4b5b04aa10883fdf10d8a5aeeb366303ab73
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2025-05-16 12:17:05 +0600

    add Auth API

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from "express";
+import User from "../models/user.model";
+import bcryptjs from "bcryptjs";
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+
+export const register=async (req:Request,res:Response): Promise<void>=>{
+  const {name,email,password,role,companyId}=req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(StatusCodes.BAD_REQUEST)
+         .json({ success:false,message: "User already exists" });
+      return;
+    }
+    const salt= await bcryptjs.genSalt(10);
+    const hashPassword=await bcryptjs.hash(password,salt);
+    const CompanyId=role === 'employee' ? companyId : null;
+    const user= await User.create({name,email,password:hashPassword,role,companyId:CompanyId});
+    res.status(StatusCodes.CREATED)
+              .json({
+                success:true,
+                message:"User register successfully",
+                data:user
+              });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+              .json({
+                success:false,
+                message:"something went wrong while registering user",
+                data:{},
+                error:error
+              });
+  }
+}
+
+export const login=async(req:Request,res:Response)=>{
+  try {
+    const {email,password}=req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(StatusCodes.BAD_REQUEST)
+         .json({ success:false,message: "Invalid email or password" });
+      return;
+    }
+    const valid=await bcryptjs.compare(password,user.password!);
+    if(!valid){
+      res.status(StatusCodes.BAD_REQUEST)
+         .json({ success:false,message: "Invalid email or password" });
+      return;
+    }
+    const payload={
+      userId:user._id,
+      email:user.email,
+      role:user.role
+    }
+    const JWT_SECRET=process.env.JWT_SECRET!;
+    const token= jwt.sign(payload,JWT_SECRET,{expiresIn:'7d'});
+    res.status(StatusCodes.OK)
+              .json({
+                success:true,
+                message:"User login successfully",
+                data:token
+              });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+              .json({
+                success:false,
+                message:"something went wrong while logining user",
+                data:{},
+                error:error
+              });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import {connect} from "./config/db";
+import apiRoute from "./routes/index"
 
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
+app.use('/api',apiRoute);
 
 const PORT=process.env.PORT || 3000;
 const DB_URL=process.env.MONGODB_URL!;
